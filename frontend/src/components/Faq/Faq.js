0509a634c1ccf434ce6c7eb6739066b13a085de5
jagadish paddada <saikumarpaidi1999@gmail.com>
2024-02-12 16:52:11 +0530

    content adding

--- frontend/src/components/Faq/Faq.js
+++ frontend/src/components/Faq/Faq.js
@@ -1,75 +1,70 @@
-import React from 'react'
-import "./Faq.css"
+import React from 'react';
+import "./Faq.css";
+
 export default function Faq() {
   return (
-    <div>
-         <section class="faq">
-    <div class="container">
-      <div class="row">
-        <div class="col-lg-12">
-          <h1 class="faq-section"> FAQ </h1>
-        </div>
-        <div class="col-lg-6 col-md-12 col-sm-12">
-          <div class="accordion" id="accordionExample">
-            <div class="accordion-item faq-accordances">
-              <h2 class="accordion-header" id="headingOne">
-                <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-                  Accordion Item #1
-                </button>
-              </h2>
-              <div id="collapseOne" class="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
-                <div class="accordion-body">
-                  <strong>This is the first item's accordion body.</strong> It is shown by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+    <section className="faq">
+      <div className="container">
+        <h1 className="faq-section"> FAQ </h1>
+        <div className="row">
+          <div className="col-lg-6 col-md-12 col-sm-12">
+            <div className="accordion" id="accordionExample">
+              {faqItems.map((item, index) => (
+                <div className="accordion-item faq-accordances" key={index}>
+                  <h2 className="accordion-header" id={`heading${index}`}>
+                    <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target={`#collapse${index}`} aria-expanded="false" aria-controls={`collapse${index}`}>
+                      <strong>{item.question}</strong>
+                    </button>
+                  </h2>
+                  <div id={`collapse${index}`} className="accordion-collapse collapse" aria-labelledby={`heading${index}`} data-bs-parent="#accordionExample">
+                    <div className="accordion-body">
+                      {item.answer}
+                    </div>
+                  </div>
                 </div>
-              </div>
-            </div>
-            <div class="accordion-item faq-accordances">
-              <h2 class="accordion-header" id="headingTwo">
-                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
-                  Accordion Item #2
-                </button>
-              </h2>
-              <div id="collapseTwo" class="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#accordionExample">
-                <div class="accordion-body">
-                  <strong>This is the second item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
-                </div>
-              </div>
-            </div>
-            <div class="accordion-item faq-accordances">
-              <h2 class="accordion-header" id="headingThree">
-                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
-                  Accordion Item #3
-                </button>
-              </h2>
-              <div id="collapseThree" class="accordion-collapse collapse" aria-labelledby="headingThree" data-bs-parent="#accordionExample">
-                <div class="accordion-body">
-                  <strong>This is the third item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
-                </div>
-              </div>
+              ))}
             </div>
           </div>
-        </div>
-        <div class="col-lg-6 col-md-12 col-sm-12">
-          <div class="make-your-questions">
-            <form>
-              <div class="mb-3">
-                <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="Name"/>
-              </div>
-              <div class="mb-3">
-                <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="Email"/>
-              </div>
-              <div class="mb-3">
-                <textarea class="form-control" id="exampleFormControlTextarea1" rows="3" placeholder="Write Something"></textarea>
-              </div>
-              <div class="mb-3">
-                <button type="button" class="btn btn-primary send-req"> SEND REQUEST </button>
-              </div>
-            </form>
+          <div className="col-lg-6 col-md-12 col-sm-12">
+            <div className="make-your-questions">
+              <form>
+                <input type="text" className="form-control mb-3" placeholder="Name"/>
+                <input type="text" className="form-control mb-3" placeholder="Email"/>
+                <textarea className="form-control mb-3" rows="3" placeholder="Write Something"></textarea>
+                <button type="button" className="btn btn-primary send-req"> SEND REQUEST </button>
+              </form>
+            </div>
           </div>
         </div>
       </div>
-    </div>
-   </section>
-    </div>
-  )
+    </section>
+  );
 }
+
+const faqItems = [
+  {
+    question: "How does HypeSquad.ai improve my engagement with viewers?",
+    answer: "HypeSquad.ai uses AI to analyze YouTube comments, enabling personalized responses, understanding viewer sentiments, and generating content ideas that resonate with your audience."
+  },
+  {
+    question: "Can HypeSquad.ai help me understand my audience better?",
+    answer: "Absolutely! Our sentiment analysis tools dive deep into comments to reveal what viewers truly feel about your content, offering invaluable insights into their preferences."
+  },
+  {
+    question: "Will using HypeSquad.ai increase my channel's viewership and subscriber count?",
+    answer: "While direct outcomes vary, our tools are designed to significantly enhance viewer satisfaction and engagement, which are critical factors in growing your viewership and subscriber base."
+  },
+  {
+    question: "Is HypeSquad.ai suitable for new YouTubers?",
+    answer: "Yes, our platform is user-friendly and beneficial for YouTubers at any stage of their career, providing tools to engage and grow your audience from the start."
+  },
+  {
+    question: "How does the content ideas feature work?",
+    answer: "By analyzing audience feedback and trends in your comments, HypeSquad.ai identifies topics and themes that your viewers are interested in, helping you to create more targeted and engaging content."
+  }
+  ,
+  {
+    question: "What makes HypeSquad.ai different from other engagement tools?",
+    answer: "Our focus on leveraging AI for direct comment engagement, detailed sentiment analysis, and actionable content insights sets us apart, offering a comprehensive solution for YouTube influencers to deepen viewer relationships and optimize their content strategy"
+  },
+];
